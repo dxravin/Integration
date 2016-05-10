a65c0b5abf45c22c9d6aec4979176816bf74c482
dxravin <deepthi.ravindran@centurylink.com>
2016-05-10 12:12:39 +0530

    Updated the test case to Ultimatix

--- src/IntegrationPackage/IntegrationClass.java
+++ src/IntegrationPackage/IntegrationClass.java
@@ -2,6 +2,7 @@ package IntegrationPackage;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
@@ -13,9 +14,13 @@ public class IntegrationClass {
 	
 	@Test
 	public void main() {
-		driver.get("https://services.ultra.me/login.php");
-	    driver.manage().window().maximize();
-	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		 driver.get("https://www.ultimatix.net/");
+		  driver.manage().window().maximize();
+		  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		  driver.findElement(By.id("USER")).sendKeys("314374");
+		  driver.findElement(By.id("PASSWORD")).sendKeys("5thMonth!");
+		  driver.findElement(By.id("login_button")).click();
+		  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	     	}
 	
 	@AfterTest
